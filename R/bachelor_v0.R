5a1a77074b4b95cefba2648a5b824e1263390474
frejaaugustesen <ucloud@j-5181940-job-0.j-5181940.ucloud-apps.svc.cluster.local>
2025-02-28 11:26:12 +0100

    loading correct data and detalis after meeting with supervisor

--- R/bachelor_v0.R
+++ R/bachelor_v0.R
@@ -10,78 +10,15 @@ source(here::here("R/package_load.R"))
 
 set.seed(100)
 
-# installing packages ----------------------------------------------------
-
-## BiocManager 
-install.packages("BiocManager")
-library(BiocManager) # added to package_load
-
-## scDblFinder - skal bruge hjælp
-BiocManager::install("scDblFinder")
-library(scDblFinder) # virker ikke??
-BiocManager::install("plger/scDblFinder")
-
-## Seurat
-install.packages("Seurat")
-library(Seurat) # added to package_load
-
-## Jointly - skal bruge hjælp
-install.packages("remotes")
-remotes::install_github("madsen-lab/rJOINTLY")
-library(rJOINTLY) # virker ikke??
-
-## qs
-install.packages("qs")
-library(qs) # added to package_load
-
-## XGBoost - understående er copy paste fra geeksforgeeks.org
-# (XGBoost) Installing Packages 
-install.packages("data.table") 
-install.packages("dplyr") 
-install.packages("ggplot2") 
-install.packages("caret") 
-install.packages("xgboost") 
-install.packages("e1071") 
-install.packages("cowplot") 
-
-# (XGBoost) Loading packages 
-library(data.table) # for reading and manipulation of data 
-library(dplyr)     # for data manipulation and joining 
-library(ggplot2) # for plotting 
-library(caret)     # for modeling 
-library(xgboost) # for building XGBoost model 
-library(e1071)     # for skewness 
-library(cowplot) # for combining multiple plots 
-
-# loading sander seurat object -----------------------------------------------
-sander <- qread("data/seurat_objects/wang_sander_merged.qs")
-
-str(sander)
-head(sander@assays$RNA@layers$counts)
-sander[["RNA"]]$counts[1:10, 1:10] %>%
-  as.matrix()
-
-library(Seurat)
-sander <- NormalizeData(sander)
-
-head(sander@assays$RNA@layers$data)
-sander[["RNA"]]$data[1:20, 1:10] %>%
-  as.matrix()
-
-str(sander)
-
-sander[["RNA"]]
-
-
-# motakis sample islet28 --------------------------------------------------
+# islet28 load (motakis) --------------------------------------------------
 
 # using motakis Islet28
 # folder for Islet 28: /data_raw/motakis/Islet28/Solo.out/GeneFull/raw
 
 # loading data
-islet28.mtx <- ReadMtx("data_raw/motakis/Islet28/Solo.out/GeneFull/raw/matrix.mtx", 
-                   "data_raw/motakis/Islet28/Solo.out/GeneFull/raw/barcodes.tsv",
-                   "data_raw/motakis/Islet28/Solo.out/GeneFull/raw/features.tsv")
+islet28.mtx <- ReadMtx("data_raw/motakis/Islet28/Solo.out/Gene/raw/matrix.mtx", 
+                   "data_raw/motakis/Islet28/Solo.out/Gene/raw/barcodes.tsv",
+                   "data_raw/motakis/Islet28/Solo.out/Gene/raw/features.tsv")
 
 # find empty droplets
 remotes::install_github("madsen-lab/valiDrops")
@@ -98,11 +35,12 @@ counts.subset <- islet28.mtx[, colnames(islet28.mtx) %in% rank.pass]
 
 # creating seurat object
 
-islet28 <- CreateSeuratObject(counts.subset)
+islet28 <- CreateSeuratObject(counts.subset, project = "islet28") #HUSK
 
 saveRDS(islet28, file = here::here("data/seurat_objects/motakis/islet28.rds"))
+islet28 <- readRDS(here::here("data/seurat_objects/motakis/islet28.rds"))
 
-# Islet28 QC (motakis) -----------------------------------------------------¨
+# Islet28 QC (motakis) -----------------------------------------------------
 
 islet28[["percent.mt"]] <- PercentageFeatureSet(islet28, pattern = "^MT-")
 
@@ -113,28 +51,84 @@ head(islet28.df)
 
 ### nCount_RNA
 ggplot(data = islet28.df, aes(x=nCount_RNA)) +
-  geom_histogram(bins = 100)
+  geom_histogram(bins = 200) + 
+  geom_vline(xintercept = 1500)
+
 
 ### nFeatures_RNA
-ggplot(data = islet28.df, aes(x=nFeature_RNA)) +
-  geom_histogram(bins = 100)
+ggplot(data = islet28.df, aes(x=nFeature_RNA) +
+         geom_histogram(bins = 100)))
+  
 
 ### percent.mt
 ggplot(data = islet28.df, aes(x=percent.mt)) +
-  geom_histogram(bins = 100)
+  geom_histogram(bins = 100) +
+  geom_vline(xintercept = 15)
+
 
 ## subsetting data ----
 # remember to asses histograms when choosing thresholds
-islet28 <- subset(islet28, subset = nFeature_RNA > 900 &
-                    nFeature_RNA < 7000 & percent.mt < 15 &
-                    nCount_RNA > 9000, nCount_RNA < 40000)
+islet28 <- subset(islet28, subset = nFeature_RNA >= 1000 & 
+                    percent.mt < 15 &
+                    nCount_RNA >= 1500 & nCount_RNA <= 40000)
 
-## doublets ----
+## kompleksitet af celler (nFeature/nCount) ----
+islet28@meta.data$log10complexity <- log10(islet28@meta.data$nFeature_RNA)/
+  log10(islet28@meta.data$nCount_RNA)
+
+hist(islet28@meta.data$log10complexity, )
+
+ggplot(data = islet28@meta.data, aes(x=log10complexity)) +
+  geom_histogram(bins = 100) +
+  geom_vline(xintercept = 0.80, col = "red")
+
+## doublets (udskudt) ----
 
 ## marker genes ----
 
 ## clustering of cells ----
 
 
+#  pancreas azimuth -------------------------------------------------------
 
+panc_data <-readRDS("test_data/enge.rds")
+str(panc_data)
+
+panc_df <- as.data.frame(panc_data@meta.data)
+
+ggplot(data = panc_df, aes(x=nCount_RNA)) +
+  geom_histogram(bins = 100)
+
+ggplot(data = panc_df, aes(x=nFeature_RNA)) +
+  geom_histogram(bins = 100)
+
+head(panc_data@assays$RNA$data, 10)
+
+# using guide
+
+panc_data <- FindVariableFeatures(panc_data, 
+                                  selection.method = "vst", nfeatures = 2000)
+
+top10 <- head(VariableFeatures(panc_data), 10)
+
+plot1 <- VariableFeaturePlot(panc_data)
+plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
+plot1 + plot2
+
+# scaling data
+all.genes <- rownames(panc_data)
+panc_data <- ScaleData(panc_data, features = all.genes)
+
+# pca
+panc_data <- RunPCA(panc_data, features = VariableFeatures(object = panc_data))
+
+ElbowPlot(panc_data)
+
+# clustering
+panc_data <- FindNeighbors(panc_data, dims = 1:13)
+panc_data <- FindClusters(panc_data, resolution = 0.5)
+
+panc_data <- RunUMAP(panc_data, dims = 1:13)
+
+DimPlot(panc_data, reduction = "umap")
 
---
+++ R/intro_and_package_install.R
@@ -0,0 +1,66 @@
+# introduction and package installation script
+
+source(here::here("R/set_up.R"))
+
+# installing packages ----------------------------------------------------
+
+## BiocManager 
+install.packages("BiocManager")
+library(BiocManager) # added to package_load
+
+## scDblFinder - skal bruge hjælp
+BiocManager::install("scDblFinder")
+library(scDblFinder) # virker ikke??
+BiocManager::install("plger/scDblFinder")
+
+## Seurat
+install.packages("Seurat")
+library(Seurat) # added to package_load
+
+## Jointly - skal bruge hjælp
+install.packages("remotes")
+remotes::install_github("madsen-lab/rJOINTLY")
+library(rJOINTLY) # virker ikke??
+
+## qs
+install.packages("qs")
+library(qs) # added to package_load
+
+## XGBoost - understående er copy paste fra geeksforgeeks.org
+# (XGBoost) Installing Packages 
+install.packages("data.table") 
+install.packages("dplyr") 
+install.packages("ggplot2") 
+install.packages("caret") 
+install.packages("xgboost") 
+install.packages("e1071") 
+install.packages("cowplot") 
+
+# (XGBoost) Loading packages 
+library(data.table) # for reading and manipulation of data 
+library(dplyr)     # for data manipulation and joining 
+library(ggplot2) # for plotting 
+library(caret)     # for modeling 
+library(xgboost) # for building XGBoost model 
+library(e1071)     # for skewness 
+library(cowplot) # for combining multiple plots 
+
+# loading sander seurat object -----------------------------------------------
+sander <- qread("data/seurat_objects/wang_sander_merged.qs")
+
+str(sander)
+head(sander@assays$RNA@layers$counts)
+sander[["RNA"]]$counts[1:10, 1:10] %>%
+  as.matrix()
+
+library(Seurat)
+sander <- NormalizeData(sander)
+
+head(sander@assays$RNA@layers$data)
+sander[["RNA"]]$data[1:20, 1:10] %>%
+  as.matrix()
+
+str(sander)
+
+sander[["RNA"]]
+
