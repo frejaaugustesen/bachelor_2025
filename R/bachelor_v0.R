6b78462b8ab5a0bba8acc929ab4543bee33a2cde
frejaaugustesen <ucloud@j-5182746-job-0.j-5182746.ucloud-apps.svc.cluster.local>
2025-03-03 11:01:44 +0100

    Identifying celltypes in clusters in islet28

--- R/bachelor_v0.R
+++ R/bachelor_v0.R
@@ -35,6 +35,7 @@ islet28 <- CreateSeuratObject(counts.subset, project = "islet28") #HUSK
 
 saveRDS(islet28, file = here::here("data/seurat_objects/motakis/islet28.rds"))
 islet28 <- readRDS(here::here("data/seurat_objects/motakis/islet28.rds"))
+islet28 <- readRDS(here::here("data/seurat_objects/motakis/islet28/islet28_QC.rds"))
 
 # Islet28 QC (motakis) -----------------------------------------------------
 
@@ -102,14 +103,14 @@ ElbowPlot(islet28) # 1:16
 
 ## marker genes ----
 islet28 <- FindNeighbors(islet28, dims = 1:16)
-islet28 <- FindClusters(islet28, resolution = 0.6)
+islet28 <- FindClusters(islet28, resolution = 0.4)
 
 head(Idents(islet28), 5)
 
 ## clustering of cells ----
 islet28 <- RunUMAP(islet28, dims = 1:16)
 
-DimPlot(islet28, reduction = "umap")
+DimPlot(islet28, reduction = "umap", label = TRUE)
 
 # find markers
 islet28.markers <- FindAllMarkers(islet28, only.pos = TRUE)
@@ -121,38 +122,126 @@ islet28.markers %>%
   group_by(cluster) %>%
   dplyr::filter(avg_log2FC > 1)
 
-VlnPlot(islet28, features = c("CYSTM1", "CRYBA2"))
+islet28.markers %>%
+  group_by(cluster) %>%
+  slice_max(n = 2, order_by = avg_log2FC)
 
-# FeaturePlots
+FeaturePlot(islet28, features = c("ADCYAP1"),  min.cutoff = "q10")
 
-FeaturePlot(islet28, features = "RPL21")
 
-FeaturePlot(islet28, features = beta)
+### cluster 0 ----
+cluster0.markers <- FindMarkers(islet28, ident.1 = 0, only.pos = TRUE)
+head(cluster0.markers, n = 5) 
+# possible beta cells (see insulin, HADH, NPTX2)
+FeaturePlot(islet28, features = c("SAMD11","ADCYAP1", "MEG3", 
+                                  "NPTX2", "HADH", "INS"),  min.cutoff = "q20")
 
-# markers for each cluster (tror at cluster 1, 4 og 6 er beta)
-# cluster nul anderledes 1 og 2
-cluster0.markers <- FindMarkers(islet28, ident.1 = 0)
-head(cluster0.markers, n = 5) # måske PPP1R1A (alfa)
+# er det et enkelt cluster eller burde resolution være lavere?
+cluster0.markers_comp <- FindMarkers(islet28, ident.1 = 0, ident.2 = c(10, 5), 
+                                     only.pos = TRUE)
+head(cluster0.markers_comp, n = 5) 
+FeaturePlot(islet28, features = c("RPL34","RPS27A", "RPL24", 
+                                  "RPL35A", "RPS4X"),  min.cutoff = "q20")
+# generne der adskiller cluster 0 fra 10 og 5 er ikke marker gener hos Azimuth
+# samt de er udtrykt hos mange clustre
+# måske der burde være lavere resolution således at både 0, 5 og 10 er beta
 
-# beta
-beta_markers_in_clusters <- islet28.markers[islet28.markers$gene 
-                                            %in% beta, ]
-table(beta_markers_in_clusters$cluster) # 1, 11 og 12 (6 og 4)
+### cluster 1 ----
+cluster1.markers <- FindMarkers(islet28, ident.1 = 1)
+head(cluster1.markers, n = 5) 
+FeaturePlot(islet28, features = c("C5orf38","AC099509.1", "PPP1R1A", 
+                                  "C12orf75", "CRYBA2"),  min.cutoff = "q20")
+# måske aplha (PPP1R1A og CRYBA2)
 
-# aplha
-alpha_markers_in_clusters <- islet28.markers[islet28.markers$gene 
-                                            %in% alpha, ]
-table(alpha_markers_in_clusters$cluster) # 0 (3)
+# er det et enkelt cluster eller burde resolution være lavere?
+cluster1.markers_comp <- FindMarkers(islet28, ident.1 = 1, ident.2 = c(4, 2), 
+                                     only.pos = TRUE)
+head(cluster1.markers_comp, n = 5) 
+FeaturePlot(islet28, features = c("SERF2","TUBA1B", "COX8A", 
+                                  "COX17", "C12orf75"),  min.cutoff = "q20")
 
-# delta
-delta_markers_in_clusters <- islet28.markers[islet28.markers$gene 
-                                             %in% delta, ]
-table(delta_markers_in_clusters$cluster) # 5 og 12
+# igen - resolution bør muligvis være lavere da de to andre clustre ikke skiller
+# sig synderligt meget ud
 
-# gamma
-gamma_markers_in_clusters <- islet28.markers[islet28.markers$gene 
-                                             %in% gamma, ]
-table(gamma_markers_in_clusters$cluster) # 5
+### cluster 3 ----
+cluster3.markers <- FindMarkers(islet28, ident.1 = 3)
+head(cluster3.markers, n = 5) 
+FeaturePlot(islet28, features = c("LY6H","BCHE", "CALB1", 
+                                  "GPC5-AS1", "HHEX"),  min.cutoff = "q20")
+# delta (HHEX og LY6H) gamma (GPC5-AS1) - skal den så rigtigt være delt op i 2?
+
+### cluster 6 ----
+cluster6.markers <- FindMarkers(islet28, ident.1 = 6)
+head(cluster6.markers, n = 5) 
+FeaturePlot(islet28, features = c("DEFB1","TACSTD2", "PDZK1IP1", 
+                                  "KRT7", "SERPING1"),  min.cutoff = "q20")
+
+# epsilon (DEFB1) ductal (KRT7 og SERPING1)
+# nogle af dem udtrykkes også i især cluster 9 (og 8/7)
+
+### cluster 7 ----
+cluster7.markers <- FindMarkers(islet28, ident.1 = 7)
+head(cluster7.markers, n = 5) 
+FeaturePlot(islet28, features = c("COL6A3","COL15A1", "SFRP2", 
+                                  "COL6A2", "COL3A1"),  min.cutoff = "q20")
+# activated stelat (COL6A3, SFRP2 og COL3A1)
+# ser rimelig isoleret ud til cluster 7 og altså ikke cluster 8 med
+
+### cluster 8 ----
+cluster8.markers <- FindMarkers(islet28, ident.1 = 8)
+head(cluster8.markers, n = 5) 
+FeaturePlot(islet28, features = c("ESAM","FABP4", "GPR4", 
+                                  "ECSCR", "CD93"),  min.cutoff = "q20")
+# quiescent_stellate (ESAM, FABP4)
+
+### cluster 9 ----
+cluster9.markers <- FindMarkers(islet28, ident.1 = 9)
+head(cluster9.markers, n = 5) 
+FeaturePlot(islet28, features = c("CXCL17","GSTA2", "ALDOB", 
+                                  "ANPEP", "CPA2"),  min.cutoff = "q20")
+# ingen matches hos Azimuth - kan læse mig til at CPA2 har noget med pancreas at gøre
+
+### cluster 11 ----
+cluster11.markers <- FindMarkers(islet28, ident.1 = 11)
+head(cluster11.markers, n = 5) 
+FeaturePlot(islet28, features = c("LAPTM5","TYROBP", "FCER1G", 
+                                  "C1QC", "MS4A7"),  min.cutoff = "q20")
+# immune (LAPTM5, TYROBP, FCER1G og C1QC)
+
+### cluster 12 ----
+cluster12.markers <- FindMarkers(islet28, ident.1 = 12)
+head(cluster12.markers, n = 5) 
+FeaturePlot(islet28, features = c("UBE2C","PBK", "NUSAP1", 
+                                  "HJURP", "AURKB"),  min.cutoff = "q20")
+# cycling (UBE2C og PBK)
+
+### konklusion ----
+# ønsker en resolution hvor cluster 0, 5 og 10 er en
+# 4, 1 og 2 samles
+# altså 9 clusters i alt
+# ved forsøg med at sænke resolution forsvinder cluster 12 som er cyclin først
+# og cluster 8/7 bliver til en 
+# så jeg kan ikke få den opdeling som jeg regnede med
+
+### cluster ID ----
+# cluster 10, 0 og 5 = beta
+# cluster 1, 4 og 2 = alpha
+# cluster 3 = delta/gamma
+# cluster 6 = epsilon/ductal
+# cluster 7 = activated stelat
+# cluster 8 = quiescent_stellate
+# cluster 9 = ?
+# cluster 11 = immune
+# cluster 12 = cycling
+
+new.cluster.ids <- c("beta_1", "alpha_1", "alpha_2", "delta/gamma", "alpha_3", 
+                     "beta_2", "epsilon/ductal", "activated_stellate",
+                     "quiescent_stellate", "?", "beta_3", "immune", "cycling")
+
+names(new.cluster.ids) <- levels(islet28)
+islet28 <- RenameIdents(islet28, new.cluster.ids)
+DimPlot(islet28, reduction = "umap", label = TRUE, 
+        label.size = 3, pt.size = 0.3, repel = TRUE) + NoLegend()
 
 
 # random sample selection -------------------------------------------------
